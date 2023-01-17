ad4e51a052046454967f581322f114c7b2cddb8d
idriss30 <idriss.cissoko@hotmail.com>
2023-01-17 14:13:38 -0500

    added new pages to webpack config

--- utility/webpack.common.js
+++ utility/webpack.common.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const Dotenv = require("dotenv-webpack");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 const BundleAnalyzerPlugin =
@@ -10,6 +11,16 @@ module.exports = {
   entry: {
     index: {
       import: path.resolve(__dirname, "..", "./src/index.js"),
+    },
+    article: {
+      import: path.resolve(__dirname, "..", "./src/article.js"),
+    },
+    searchResult: {
+      import: path.resolve(__dirname, "..", "./src/searchResult.js"),
+    },
+
+    singleRecipe: {
+      import: path.resolve(__dirname, "..", "./src/singleRecipe.js"),
     },
   },
 
@@ -26,11 +37,31 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "..", "./src/index.html"),
       filename: "index.html",
+      chunks: ["index"],
+      inject: "body",
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "..", "./src/article.html"),
+      filename: "article.html",
+      chunks: ["article"],
+      inject: "body",
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "..", "./src/searchResult.html"),
+      filename: "searchResult.html",
+      chunks: ["searchResult"],
+      inject: "body", //inject javascript in the body
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "..", "./src/singleRecipe.html"),
+      filename: "singleRecipe.html",
+      chunks: ["singleRecipe"],
       inject: "body",
     }),
     new FaviconsWebpackPlugin({
       logo: path.resolve(__dirname, "..", "./src/favico.png"),
     }),
+    new Dotenv(),
   ],
   module: {
     rules: [
