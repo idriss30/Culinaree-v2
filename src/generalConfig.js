c9aef4a8ca0cba3e3baffad366c6273031791c03
idriss30 <idriss.cissoko@hotmail.com>
2023-01-17 14:10:36 -0500

    moved general config to own files

---
+++ src/generalConfig.js
@@ -0,0 +1,33 @@
+const axios = require("axios");
+export const scrollTotop = () => {
+  window.scrollTo(0, 0);
+};
+
+export function getParams(name) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(name);
+}
+
+export async function fetchData(url, endpoint) {
+  const fetchResponse = await axios.get(url + endpoint);
+  return fetchResponse;
+}
+
+// create a function to change key
+export function generateKey() {
+  //key
+  let keys = [
+    // keys are stored here for demontration purpose only
+    `${process.env.keyOne}`,
+    `${process.env.keyTwo}`,
+    `${process.env.keyThree}`,
+  ];
+  let index = Math.floor(Math.random() * 3);
+  return keys[index];
+}
+
+export function navLinksRedirect(element) {
+  element.addEventListener("click", () => {
+    window.location.replace(`/searchResult.html?id=${element.id}`);
+  });
+}
